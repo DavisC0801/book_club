d9869ec52149ceb3778426348bfc1cd52bd02c0c
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-06 17:03:22 -0600

    Add WIP test setup for books/index
    Co-Authored-By: Christopher Davis <DavisC0801@users.noreply.github.com>

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor", type: :feature do
   describe "when I visit the books index page" do
+    before(:each) do
+      @book_1 = Book.create!(title: "The Frozen Deep", page_count: 106, year_published: 1874, thumbnail: "https://images.gr-assets.com/books/1328728986l/1009218.jpg")
+      # TO DO: add author(s)
+    end
+
     it "loads the page" do
       visit "/books"
 
@@ -14,13 +19,12 @@ RSpec.describe "As a visitor", type: :feature do
 
       within "#book-#{@book_1.id}" do
         expect(page).to have_content(@book_1.title)
-        expect(page).to have_content(@book_1.authors.first.name)
-        expect(page).to have_content(@book_1.authors.second.name)
+        # expect(page).to have_content(@book_1.authors.first.name)
+        # expect(page).to have_content(@book_1.authors.second.name)
         expect(page).to have_content("#{@book_1.page_count} pages")
         expect(page).to have_content("Published in #{@book_1.year_published}")
         expect(page).to have_css("img [src*='#{@book_1.thumbnail}']")
       end
-
     end
   end
 end
