367b604bdbb5b788fa52390a5d55d07810f1970f
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-06 16:51:55 -0600

    Add feature test for User Story 6
    Co-Authored-By: Christopher Davis <DavisC0801@users.noreply.github.com>

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "As a visitor" do
+RSpec.describe "As a visitor", type: :feature do
   describe "when I visit the books index page" do
     it "loads the page" do
       visit "/books"
 
       expect(page.status_code).to eq(200)
       expect(current_path).to eq("/books")
+    end
+
+    it "shows book information for all books" do
+      visit "/books"
+
+      within "#book-#{@book_1.id}" do
+        expect(page).to have_content(@book_1.title)
+        expect(page).to have_content(@book_1.authors.first.name)
+        expect(page).to have_content(@book_1.authors.second.name)
+        expect(page).to have_content("#{@book_1.page_count} pages")
+        expect(page).to have_content("Published in #{@book_1.year_published}")
+        expect(page).to have_css("img [src*='#{@book_1.thumbnail}']")
+      end
+
     end
   end
 end
