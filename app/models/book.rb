4258a140e2e394b6c75fbff00ce774a8bff3e856
davisc0801 <davisc1014@gmail.com>
2019-05-07 11:58:20 -0600

    Add book_author model and relationships to author and book models
    Co-Authored-By: Alexandra Chakeres <chakeresa@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
   validates_presence_of :title
   validates_presence_of :page_count
   validates_presence_of :year_published
