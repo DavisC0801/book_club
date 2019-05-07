dfc8ce1ca527cfa8a91d1f8083af62a4c2a2d88d
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-07 16:12:22 -0600

    Add migration to add users table
    Co-Authored-By: Christopher Davis <DavisC0801@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190507220841) do
+ActiveRecord::Schema.define(version: 20190507221133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 20190507220841) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_reviews_on_book_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "book_authors", "authors"
