9c97c15c9aa227e9af70777e94bc25bc01af6d8d
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-07 16:25:20 -0600

    Add migration for user_id foreign key in reviews table
    Co-Authored-By: Christopher Davis <DavisC0801@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190507221133) do
+ActiveRecord::Schema.define(version: 20190507222235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,9 @@ ActiveRecord::Schema.define(version: 20190507221133) do
     t.bigint "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,4 +60,5 @@ ActiveRecord::Schema.define(version: 20190507221133) do
   add_foreign_key "book_authors", "authors"
   add_foreign_key "book_authors", "books"
   add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
