47b8e2ccabe74c336cf95fc8d97dda8de553f411
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-07 15:48:22 -0600

    Test feature to show book & review info on the book's show page
    Co-Authored-By: Christopher Davis <DavisC0801@users.noreply.github.com>

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe "As a visitor", type: :feature do
   before(:each) do
     @book_1 = Book.create!(title: "The Frozen Deep", page_count: 106, year_published: 1874, thumbnail: "https://images.gr-assets.com/books/1328728986l/1009218.jpg")
+    @book_2 = Book.create!(title: "To Kill a Mockingbird", page_count: 281, year_published: 1960, thumbnail: "https://upload.wikimedia.org/wikipedia/en/7/79/To_Kill_a_Mockingbird.JPG")
     @author_1 = @book_1.authors.create!(name: "Wilkie Collins")
     @author_2 = @book_1.authors.create!(name: "Charles Dickens")
+    @author_3 = @book_2.authors.create!(name: "Harper Lee")
   end
 
   it "loads the page" do
@@ -12,5 +14,26 @@ RSpec.describe "As a visitor", type: :feature do
 
     expect(page.status_code).to eq(200)
     expect(current_path).to eq("/books/#{@book_1.id}")
+  end
+
+  it "shows information for a particular book" do
+    visit "/books/#{@book_1.id}"
+
+    within "#book-show" do
+      expect(page).to have_content(@book_1.title)
+      expect(page).to have_content(@author_1.name)
+      expect(page).to have_content(@author_2.name)
+      expect(page).to have_content(@book_1.page_count)
+      expect(page).to have_content(@book_1.year_published)
+      expect(page).to have_css("img[src*='#{@book_1.thumbnail}']")
+
+      expect(page).to_not have_content(@book_2.title)
+    end
+
+    # within "#review-list" do
+    #   expect(page).to have_content(@review_1.title)
+    #   expect(page).to have_content(@review_1.rating)
+    #   expect(page).to have_content(@review_1.text)
+    # end
   end
 end
