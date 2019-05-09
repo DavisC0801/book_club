3a0a934deb8fa3dd9369c9b208e4eee7c7b34cab
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-09 11:58:19 -0600

    Add basic feature test for reviews/new

---
+++ spec/features/reviews/new_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "as a visitor" do
+  describe "when there is a book with the id matching the URI" do
+    before(:each) do
+      @book_1 = Book.create!(title: "The Frozen Deep", page_count: 106, year_published: 1874, thumbnail: "https://images.gr-assets.com/books/1328728986l/1009218.jpg")
+    end
+
+    it "loads the page" do
+      visit new_book_review_path(@book_1)
+
+      expect(page.status_code).to eq(200)
+      expect(current_path).to eq(new_book_review_path(@book_1))
+    end
+  end
+end
