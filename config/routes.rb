6fd061ac9817d0be4ac8ee2723154cb37dfc1faa
davisc0801 <davisc1014@gmail.com>
2019-05-09 11:37:18 -0600

    Remove merge markers from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "books#index"
-<<<<<<< HEAD
   resources :books, only: [:index, :show, :new]
-=======
-  resources :books, only: [:index, :show]
   resources :authors, only: :show
->>>>>>> 399faa9319b58991bf93df929889c33559b917bd
   resources :users, only: :show
 end
