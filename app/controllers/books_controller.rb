24072572c55045b02eb20fba54d56bc6ad94c2db
davisc0801 <davisc1014@gmail.com>
2019-05-09 17:49:01 -0600

    Add additional functionality to view and controller to pass testing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,11 +19,20 @@ class BooksController < ApplicationController
     book = Book.create_with(
       year_published: book_params[:year_published],
       page_count: book_params[:page_count],
-      thumbnail: book_params[:thumbnail]
+      thumbnail: book_params[:thumbnail].presence || 'https://nnp.wustl.edu/img/bookCovers/genericBookCover.jpg'
     ).find_or_create_by(title: book_params[:title].titlecase)
-    author = book.authors.find_or_create_by(name: book_params[:authors].titlecase)
 
-    redirect_to book_path(book)
+    author_input = book_params[:authors].split(",")
+
+    author_input.each do |author|
+      book.authors.find_or_create_by(name: author.titlecase.strip)
+    end
+
+    if book != Book.last
+      flash[:notice] = "This book has already been created"
+    else
+      redirect_to book_path(book)
+    end
   end
 
   private
