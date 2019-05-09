609b11111ac643a69c65e9286999e71f6b594d32
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-09 15:30:21 -0600

    Add Review model validation that rating is an integer 1-5

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,6 @@ class Review < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :rating
   validates_presence_of :text
+
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
