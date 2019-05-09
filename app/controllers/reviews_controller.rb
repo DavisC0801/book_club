d8cdf8be7121d4bf7196604f12044ecc67fabefe
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-09 12:03:24 -0600

    Redirect to books index if URI for new review is for a book id that doesn't exist

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,9 @@
 class ReviewsController < ApplicationController
   def new
+    if Book.pluck(:id).include?(params[:book_id].to_i)
+      @book = Book.find(params[:book_id])
+    else
+      redirect_to books_path
+    end
   end
 end
--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe "as a visitor" do
       expect(current_path).to eq(new_book_review_path(@book_1))
     end
   end
+
+  describe "when there is no book with the id matching the URI" do
+    before(:each) do
+      @book_1 = Book.create!(title: "The Frozen Deep", page_count: 106, year_published: 1874, thumbnail: "https://images.gr-assets.com/books/1328728986l/1009218.jpg")
+    end
+
+    it "redirects to the books index" do
+      visit new_book_review_path(@book_1.id + 50)
+
+      expect(current_path).to eq(books_path)
+    end
+  end
 end
