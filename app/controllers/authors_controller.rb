55f4ddbffd0d2af0f0c9d63ab68af26a176f6a09
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-09 22:34:46 -0600

    Destroy author and relevant books / book_authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,7 +11,15 @@ class AuthorsController < ApplicationController
   end
 
   def destroy
-    #code
+    relevant_book_authors_ids = BookAuthor.where(author_id: params[:id].to_i).pluck(:id)
+    relevant_book_authors_ids.each do |book_author_id|
+      book_id = BookAuthor.find(book_author_id).book_id
+      BookAuthor.where("book_id" => book_id).destroy_all
+      Review.where("book_id" => book_id).destroy_all
+      Book.destroy(book_id)
+    end
+    Author.destroy(params[:id].to_i)
+
     redirect_to books_path
   end
 end
