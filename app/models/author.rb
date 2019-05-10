9f64d500e0a0a3e03d48ae35b2ef0b9cc1f83cf4
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-10 12:32:39 -0600

    Move most of AuthorsController#destroy to Author::destroy_author to follow MVC

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,6 +4,17 @@ class Author < ApplicationRecord
 
   validates_presence_of :name
 
+  def self.destroy_author(author_id)
+    relevant_book_authors_ids = BookAuthor.where(author_id: author_id).pluck(:id)
+    relevant_book_authors_ids.each do |book_author_id|
+      book_id = BookAuthor.find(book_author_id).book_id
+      BookAuthor.where("book_id" => book_id).destroy_all
+      Review.where("book_id" => book_id).destroy_all
+      Book.destroy(book_id)
+    end
+    Author.destroy(author_id)
+  end
+
   def book_count
     books.count
   end
