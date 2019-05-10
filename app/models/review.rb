d7f02e9d0d1970704c7808a778f0eb42d7752e20
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-10 12:03:22 -0600

    Incorporate collaborator feedback

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,9 +2,7 @@ class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
-  validates_presence_of :title
-  validates_presence_of :rating
-  validates_presence_of :text
+  validates_presence_of :title, :rating, :text
 
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
