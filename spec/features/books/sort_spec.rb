ec7d5f41916fb7540b339218a5213a8bf453ba90
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-11 12:24:50 -0600

    Add edge case tests for book sorting

--- spec/features/books/sort_spec.rb
+++ spec/features/books/sort_spec.rb
@@ -88,4 +88,40 @@ RSpec.describe "As a visitor", type: :feature do
       expect(page.all(".book-info")[2]).to have_content(@book_3.title)
     end
   end
+  describe "edge cases" do
+    it "doesn't error out when there duplicate values to sort" do
+      @book_1 = Book.create!(title: "The Frozen Deep", page_count: 106, year_published: 1874, thumbnail: "https://images.gr-assets.com/books/1328728986l/1009218.jpg")
+      @book_2 = Book.create!(title: "To Kill a Mockingbird", page_count: 600, year_published: 1960, thumbnail: "https://upload.wikimedia.org/wikipedia/en/7/79/To_Kill_a_Mockingbird.JPG")
+      @book_3 = Book.create!(title: "50 Shades of Grey", page_count: 514, year_published: 2011, thumbnail: "https://upload.wikimedia.org/wikipedia/en/5/5e/50ShadesofGreyCoverArt.jpg")
+      @author_1 = @book_1.authors.create!(name: "Wilkie Collins")
+      @author_2 = @book_1.authors.create!(name: "Charles Dickens")
+      @author_3 = @book_2.authors.create!(name: "Harper Lee")
+      user_1 = User.create(username: "Chris Davis")
+      user_2 = User.create(username: "Alexandra Chakeres")
+      @review_1 = @book_2.reviews.create!(title: "This book rocks!", rating: 5, text: "Read it!", user: user_1)
+      @review_2 = @book_1.reviews.create!(title: "This book sucks!", rating: 1, text: "Don't read it!", user: user_2)
+      @review_3 = @book_2.reviews.create!(title: "It's OK.", rating: 3, text: "Meh", user: user_2)
+      @review_4 = @book_3.reviews.create!(title: "This book sucks!", rating: 1, text: "Don't read it!", user: user_2)
+
+      visit books_path
+
+      within "#sort-bar" do
+        click_link "average rating (lowest to highest)"
+      end
+
+      expect(page).to have_content(@book_1.title)
+      expect(page).to have_content(@book_2.title)
+      expect(page).to have_content(@book_3.title)
+    end
+
+    it "doesn't error out when there are no books" do
+      visit books_path
+
+      within "#sort-bar" do
+        click_link "average rating (lowest to highest)"
+      end
+
+      expect(page).to have_content("Sort by")
+    end
+  end
 end
