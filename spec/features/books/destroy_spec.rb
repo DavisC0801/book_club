0b014412f9b3cd06a9520d11a653167c968e5d42
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-11 17:32:29 -0600

    Clean up books/destroy_spec

--- spec/features/books/destroy_spec.rb
+++ spec/features/books/destroy_spec.rb
@@ -5,10 +5,7 @@ RSpec.describe "As a visitor", type: :feature do
     before(:each) do
       @book_1 = Book.create!(title: "The Frozen Deep", page_count: 106, year_published: 1874, thumbnail: "https://images.gr-assets.com/books/1328728986l/1009218.jpg")
       @book_2 = Book.create!(title: "To Kill a Mockingbird", page_count: 281, year_published: 1960, thumbnail: "https://upload.wikimedia.org/wikipedia/en/7/79/To_Kill_a_Mockingbird.JPG")
-      @author_1 = @book_1.authors.create!(name: "Wilkie Collins")
-      @author_2 = @book_1.authors.create!(name: "Charles Dickens")
-      @author_3 = @book_2.authors.create!(name: "Harper Lee")
-      @book_3 = @author_1.books.create!(title: "The Moonstone", page_count: 528, year_published: 1868, thumbnail: "https://upload.wikimedia.org/wikipedia/commons/2/26/The_Moonstone_1st_ed.jpg")
+      @book_3 = Book.create!(title: "The Moonstone", page_count: 528, year_published: 1868, thumbnail: "https://upload.wikimedia.org/wikipedia/commons/2/26/The_Moonstone_1st_ed.jpg")
       user_1 = User.create(username: "Chris Davis")
       user_2 = User.create(username: "Alexandra Chakeres")
       @review_1 = @book_1.reviews.create!(title: "This book rocks!", rating: 5, text: "Read it!", user: user_1)
