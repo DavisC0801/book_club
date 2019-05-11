326b0c5fcc7c03981fa79ab0648dcee9a3c7b791
davisc0801 <davisc1014@gmail.com>
2019-05-10 18:22:27 -0600

    Add test for sorting reviews on a user page by creation time

---
+++ spec/features/users/sort_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+RSpec.describe "As a Visitor" do
+  describe "when I visit a users's show page" do
+    before(:each) do
+      book_1 = Book.create!(title: "The Frozen Deep", page_count: 106, year_published: 1874, thumbnail: "https://images.gr-assets.com/books/1328728986l/1009218.jpg")
+      book_2 = Book.create!(title: "To Kill a Mockingbird", page_count: 281, year_published: 1960, thumbnail: "https://upload.wikimedia.org/wikipedia/en/7/79/To_Kill_a_Mockingbird.JPG")
+      book_3 = Book.create!(title: "The Illiad", page_count: 443, year_published: 850, thumbnail: "https://images.gr-assets.com/books/1388188509l/1371.jpg")
+      @user_1 = User.create!(username: "Chris Davis")
+      @review_1 = book_1.reviews.create!(title: "Terrible", rating: 1, text: "This was the worst book I've ever read.", user: @user_1)
+      @review_2 = book_3.reviews.create!(title: "Great", rating: 5, text: "It's awesome!", user: @user_1)
+      @review_3 = book_2.reviews.create!(title: "Not good", rating: 2, text: "I wouldn't recommend it.", user: @user_1)
+    end
+
+    it "should have links to sort reviews by newest and oldest" do
+      visit user_path(@user_1)
+
+      within("#sort-bar") do
+        expect(page).to have_link("Sort by Oldest")
+        expect(page).to have_link("Sort by Newest")
+      end
+    end
+
+    it "sorts the page by newest" do
+      visit user_path(@user_1)
+
+      click_link "sort by newest"
+
+      expect(page.all(".review-info")[0])
+      expect(page.all(".review-info")[1])
+      expect(page.all(".review-info")[2])
+    end
+
+    it "sorts the page by oldest" do
+      visit user_path(@user_1)
+
+      click_link "sort by oldest"
+
+      expect(page.all(".review-info")[0])
+      expect(page.all(".review-info")[1])
+      expect(page.all(".review-info")[2])
+    end
+  end
+end
+
+# As a Visitor,
+# When I visit a user's show page
+# I should also see links to sort reviews in the following ways:
+# - sort reviews newest first (descending chronological order)
+# - sort reviews oldest first (ascending chronological order)
