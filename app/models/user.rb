7085559be82a74a9c80e7782e91aef8c7a509a8b
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-13 16:14:45 -0600

    Add User::sort_by_review_count and User::most_reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,20 @@ class User < ApplicationRecord
 
   validates_presence_of :username
 
+  def self.sort_by_review_count(ascending = true)
+    ascending ? first_or_last = 'FIRST' : first_or_last = 'LAST'
+    ascending ? descending = '' : descending = ' DESC'
+    order_arg = 'COUNT(reviews)' + descending + ' NULLS ' + first_or_last
+    User.select('users.*, COUNT(reviews)')
+        .left_joins(:reviews)
+        .group('users.id')
+        .order(order_arg)
+  end
+
+  def self.most_reviews(number)
+    sort_by_review_count(false).limit(number)
+  end
+
   def reviews_count
     reviews.count
   end
