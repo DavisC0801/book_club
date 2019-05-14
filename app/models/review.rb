a8282a2ee00c6cea20c50be476ae475bf3c60c0a
davisc0801 <davisc1014@gmail.com>
2019-05-13 18:07:28 -0600

    Update book model to sort associated reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,9 @@ class Review < ApplicationRecord
   validates_presence_of :title, :rating, :text
 
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+
+  def rating_percentage
+    (rating / 5.0 * 100).round(0).to_i
+  end
+
 end
